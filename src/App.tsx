89fe6ba48ea28d9fda101ef6eba576f954a2aea2
Bulgent <moritakakuro0305@gmail.com>
2024-04-04 20:37:58 +0900

    aaaaaaaa

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more2-branch2
+        Click on the Vite and React logos to learn more2-branch2-3aaa
       </p>
     </>
   )
